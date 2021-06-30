7f0a36d87caa4ffd11f2f1232d08443239419dc2
estucki16 <64447988+estucki16@users.noreply.github.com>
2021-06-29 20:50:05 -0600

    if statements created that work for all library function with transit from library to block groups

--- libraries_get_times_trial.R
+++ libraries_get_times_trial.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(sf)
 library(otpr)
 library(opentripplanner)
+library(progress)
 
 
 
@@ -16,16 +17,24 @@ libraries <- st_read("data/libraries.geojson") %>%
   st_transform(4326) %>%
   rename(id = ID)
 
-get_bglatlong <- function(){
-  bgcentroid <- read_csv("https://www2.census.gov/geo/docs/reference/cenpop2010/blkgrp/CenPop2010_Mean_BG49.txt")
+bgcentroid <- read_csv("https://www2.census.gov/geo/docs/reference/cenpop2010/blkgrp/CenPop2010_Mean_BG49.txt")
   
   
-  st_as_sf(bgcentroid, coords = c("LONGITUDE", "LATITUDE"), crs = 4326) %>%
-    mutate(id = str_c(STATEFP, COUNTYFP, TRACTCE, BLKGRPCE)) %>%
-    filter(COUNTYFP == "049") %>%
-    select(id, POPULATION)
-}
+bg <- st_as_sf(bgcentroid, coords = c("LONGITUDE", "LATITUDE"), crs = 4326) %>%
+  mutate(id = str_c(STATEFP, COUNTYFP, TRACTCE, BLKGRPCE)) %>%
+  filter(COUNTYFP == "049") %>%
+  select(id, POPULATION)
 
+big <- bg %>%
+  st_centroid() %>% 
+  st_transform(4326) %>%
+  mutate(
+    LATITUDE  = st_coordinates(.)[, 2],
+    LONGITUDE = st_coordinates(.)[, 1],
+  ) %>%
+  transmute(id = as.character(id), LONGITUDE, LATITUDE)   %>%
+  st_set_geometry(NULL)
+ 
 
 tib <- libraries %>%
   st_centroid() %>% 
@@ -40,6 +49,11 @@ tib <- libraries %>%
 tib
 
 total <- nrow(tib)
+totalbg <- nrow(big)
+pb <- progress_bar$new(total = total, format = "(:spin)[:bar]:percent")#progress bar
+routes <- data.frame("a", "b", "c", "d", "e", "f", "g", "h")
+names(routes) <- c("FromPlace", "ToPlace", "status", "duration", "walktime", "transitTime", "waitingtime", "transfers")
+k<- 1
 
 path_otp <- otp_dl_jar(file.path("OTP"), cache = TRUE)
 log2 <- otp_setup(otp = path_otp, dir = "otp")
@@ -47,17 +61,22 @@ otpcon <- otp_connect()
 on.exit(otp_stop(warn = FALSE))
 
 for (i in 1:total) {
-  response <- otp_get_times(otpcon, fromPlace = c(tib[i,]$LATITUDE, tib[i, ]$LONGITUDE), toPlace = c(40.24106, -111.6577), mode = "TRANSIT",detail = TRUE)
-  # If response is OK update dataframe
-  if (response$errorId == "OK") {
-    tib[i, "status"]<- response$errorId
-    tib[i, "duration"]<- response$itineraries$duration
-    tib[i, "walktime"]<- response$itineraries$walkTime
-    tib[i, "transitTime"]<- response$itineraries$transitTime
-    tib[i, "waitingtime"]<- response$itineraries$waitingTime
-    tib[i, "transfers"]<- response$itineraries$transfers
-  }else {
-    # record error
-    tib[i, "status"]<- response$errorId
+  for (j in 1:totalbg) {
+    response <- otp_get_times(otpcon, fromPlace = c(tib[i,]$LATITUDE, tib[i, ]$LONGITUDE), toPlace = c(big[j,]$LATITUDE, big[j,]$LONGITUDE), mode = "TRANSIT",detail = TRUE)
+    # If response is OK update dataframe
+    if (response$errorId == "OK") {
+        routes[k, "FromPlace"]<- tib[i,]$id
+        routes[k, "ToPlace"]<- big[j,]$id
+        routes[k, "status"]<- response$errorId
+        routes[k, "duration"]<- response$itineraries$duration
+        routes[k, "walktime"]<- response$itineraries$walkTime
+        routes[k, "transitTime"]<- response$itineraries$transitTime
+        routes[k, "waitingtime"]<- response$itineraries$waitingTime
+        routes[k, "transfers"]<- response$itineraries$transfers
+        k<-k+1
+    }else {
+      # record error
+      routes[, "status"]<- response$errorId
+    }
   }
 }
