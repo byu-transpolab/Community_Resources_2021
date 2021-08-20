53070dae3fdef3fd1ad9de253808b66a076c8000
Greg Macfarlane <gregmacfarlane@byu.edu>
2021-08-20 11:37:12 -0600

    Reorganize targets

--- _targets.R
+++ _targets.R
@@ -24,43 +24,46 @@ list(
   # block group centroids
   tar_target(bgcentroid, get_bglatlong()),
   
-  # OTP Routing data
+  # OTP Routing data ==========
   # OSM data for OTP is already constructed and committed. But this allows
   # us to bring it into the targets stream
   tar_target(osmpbf, get_osmbpf(file.path(otp_path, "osm.pbf")), format = "file"),
   tar_target(gtfs,   get_gtfs(file.path(otp_path, "gtfs.zip")), format = "file"),
   tar_target(graph,  make_graph(otp_path), format = "file"),
   
-  # parks
+  # Parks ============
   tar_target(park_polygons, get_parks("data/parks.geojson", this_crs)),
   tar_target(park_points, make_park_points(park_polygons, 1/500, this_crs)),
   tar_target(park_times, calculate_times(park_points, bgcentroid, graph)),
-  
-  #grocery stores
-  tar_target(groceries, get_groceries("data/groceries.geojson", this_crs)),
-  tar_target(grocery_times, calculate_times(groceries, bgcentroid)),
-  
-  # libraries
-  tar_target(libraries, get_libraries("data/libraries.geojson", this_crs)),
-  tar_target(library_times, calculate_times(libraries, bgcentroid, graph)),
-  
-  
-  # streetlight data libraries
-  tar_target(sl_libraries_csv, get_sl_data("data/streetlight_libraries.csv", "libraries"),
-             format = "file"),
-  tar_target(sl_libraries, read_sl_data(sl_libraries_csv)),
-  tar_target(lee_plot, plot_streetlight(sl_libraries, "Brigham Young University - Harold B. Lee Library")),
-  
-  # streetlight data parks
+  # streetlight ----
   tar_target(sl_parks_csv, get_sl_data("data/streetlight_parks.zip", "parks"),
              format = "file"),
   tar_target(sl_parks, read_sl_data(sl_parks_csv)),
   
-  #streetlight data groceries
+  # Groceries =====================
+  tar_target(groceries, get_groceries("data/groceries.geojson", this_crs)),
+  tar_target(grocery_times, calculate_times(groceries, bgcentroid)),
+  # streetlight ----
   tar_target(sl_grocery_csv, get_sl_data("data/streetlight_groceries.csv", "groceries"),
              format = "file"),
   tar_target(sl_grocery, read_sl_data(sl_grocery_csv)),
-
+  # choice data and models ---
+  
+  
+  
+  
+  # Libraries ======================
+  tar_target(libraries, get_libraries("data/libraries.geojson", this_crs)),
+  tar_target(library_times, calculate_times(libraries, bgcentroid, graph)),
+  # streetlight ----
+  tar_target(sl_libraries_csv, get_sl_data("data/streetlight_libraries.csv", "libraries"),
+             format = "file"),
+  tar_target(sl_libraries, read_sl_data(sl_libraries_csv)),
+  tar_target(lee_plot, plot_streetlight(sl_libraries, "Brigham Young University - Harold B. Lee Library")),
+  # choice data and models ---
+  tar_target(library_choicedata, )
+  
+  
   
   # combination df
   #tar_target(all_data, make_all_data(park_times, library_times, grocery_times))
