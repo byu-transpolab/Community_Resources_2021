c1c1fa634189a59b594b175d5f8d165379a8e69a
Greg Macfarlane <gregmacfarlane@byu.edu>
2021-10-14 15:42:44 -0600

    Add code for horowitz non-nested test

--- R/choice_modeling.R
+++ R/choice_modeling.R
@@ -165,83 +165,46 @@ estimate_parkmodels <- function(parks_estdata){
   models
 }
 
+my_augment <- function (x, data = x$model, ...) {
+  idx <- x$model$idx
+  reg <- x$model %>% broom:::as_augment_tibble() %>% dplyr::select(-idx) %>% 
+    dplyr::rename(chosen = 1, .probability = probabilities, 
+                  .fitted = linpred) %>% 
+    dplyr::mutate(id = idx[, 1], alternative = idx[, 2], .resid = as.vector(x$residuals)) %>% 
+    dplyr::select(id, alternative, chosen, everything())
+  reg
+}
 
-function (m1, m2, digits = getOption("digits"))  {
-  m1y <- m1$model$chosen
-  m2y <- m2$model$chosen
-  m1n <- length(m1y)
-  m2n <- length(m2y)
-  if (m1n == 0 | m2n == 0) 
-    stop("Could not extract dependent variables from models.")
-  if (m1n != m2n) 
-    stop(paste("Models appear to have different numbers of observations.\n", 
-               "Model 1 has ", m1n, " observations.\n", "Model 2 has ", 
-               m2n, " observations.\n", sep = ""))
-  if (any(m1y != m2y)) {
-    stop(paste("Models appear to have different values on dependent variables.\n"))
-  }
+
+#' Horowitz likelihood ratio test of nested specifications
+#' 
+#' @param m1 an mlogit model
+#' @param m2 an mlogit model
+#' @return P-value of the horowitz test.
+#' 
+#' 
+horowitz_lrtest <- function(m1, m2){
+  g1 <- glance(m1)
+  g2 <- glance(m2)
   
+  ll0 <- g1$nobs * log(1 / length(m1$freq))
   
-  p1 <- augment(m1)
-  p2 <- predprob(m2)
-  if (!all(colnames(p1) == colnames(p2))) {
-    stop("Models appear to have different values on dependent variables.\n")
-  }
-  whichCol <- match(m1y, colnames(p1))
-  whichCol2 <- match(m2y, colnames(p2))
-  if (!all(whichCol == whichCol2)) {
-    stop("Models appear to have different values on dependent variables.\n")
-  }
-  m1p <- rep(NA, m1n)
-  m2p <- rep(NA, m2n)
-  for (i in 1:m1n) {
-    m1p[i] <- p1[i, whichCol[i]]
-    m2p[i] <- p2[i, whichCol[i]]
-  }
-  k1 <- length(coef(m1))
-  k2 <- length(coef(m2))
-  lm1p <- log(m1p)
-  lm2p <- log(m2p)
-  m <- lm1p - lm2p
-  bad1 <- is.na(lm1p) | is.nan(lm1p) | is.infinite(lm1p)
-  bad2 <- is.na(lm2p) | is.nan(lm2p) | is.infinite(lm2p)
-  bad3 <- is.na(m) | is.nan(m) | is.infinite(m)
-  bad <- bad1 | bad2 | bad3
-  neff <- sum(!bad)
-  if (any(bad)) {
-    cat("NA or numerical zeros or ones encountered in fitted probabilities\n")
-    cat(paste("dropping these", sum(bad), "cases, but proceed with caution\n"))
-  }
-  aic.factor <- (k1 - k2)/neff
-  bic.factor <- (k1 - k2)/(2 * neff) * log(neff)
-  v <- rep(NA, 3)
-  arg1 <- matrix(m[!bad], nrow = neff, ncol = 3, byrow = FALSE)
-  arg2 <- matrix(c(0, aic.factor, bic.factor), nrow = neff, 
-                 ncol = 3, byrow = TRUE)
-  num <- arg1 - arg2
-  s <- apply(num, 2, sd)
-  numsum <- apply(num, 2, sum)
-  v <- numsum/(s * sqrt(neff))
-  names(v) <- c("Raw", "AIC-corrected", "BIC-corrected")
-  pval <- rep(NA, 3)
-  msg <- rep("", 3)
-  for (j in 1:3) {
-    if (v[j] > 0) {
-      pval[j] <- 1 - pnorm(v[j])
-      msg[j] <- "model1 > model2"
-    }
-    else {
-      pval[j] <- pnorm(v[j])
-      msg[j] <- "model2 > model1"
-    }
-  }
-  out <- data.frame(v, msg, format.pval(pval))
-  names(out) <- c("Vuong z-statistic", "H_A", "p-value")
-  cat(paste("Vuong Non-Nested Hypothesis Test-Statistic:", 
-            "\n"))
-  cat("(test-statistic is asymptotically distributed N(0,1) under the\n")
-  cat(" null that the models are indistinguishible)\n")
-  cat("-------------------------------------------------------------\n")
-  print(out)
-  return(invisible(NULL))
+  rho21 <- if(is.nan(g1$rho2)) g1$rho20 else g1$rho2
+  rho22 <- if(is.nan(g2$rho2)) g2$rho20 else g2$rho2
+  
+
+  t1 <- tidy(m1)
+  t2 <- tidy(m2)
+  
+  k1 <- nrow(t1)
+  k2 <- nrow(t2)
+  
+  q <- -1 * sqrt( -2 * (rho21 - rho22) * ll0  + (k1 - k2) )
+  
+  # p normal
+  pval <- pnorm(q, lower.tail = TRUE)
+  message("Test statistic value: ", q)
+  
+  pval
+  
 }
